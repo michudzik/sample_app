e5e0646971b196af32ec2cb0ee098604e1b29077
michudzik <michalchudzik@live.com>
2018-04-28 14:31:39 +0200

    Finish user edit, update,index and destroy actions

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,12 +7,46 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: "", email: "foo@invalid", password: "foo", password_confirmation: "bar" } }
     assert_template 'users/edit'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
+  end
+
+  test "successful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = 'Foo bar'
+    email = 'valid@foobar.com'
+    patch user_path(@user), params: { user: { name: name, email: email, password: "", password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
+  end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name, email: email, password: "", password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
+    assert_nil session[:forwarding_url]
   end
 
 end

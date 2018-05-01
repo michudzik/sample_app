7379c77aa1e674f37f22d56b7d402d4a0fd41573
michudzik <michalchudzik@live.com>
2018-05-01 12:04:25 +0200

    Add basic password reset, sends emails, doesn't check expiration date of reset_token

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'glacial-headland-26501.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
